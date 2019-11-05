32539e6c707e884c78d9b1c8241c085862da8cdb
daiki0809 <cookietanaka0809@gmail.com>
2019-11-05 04:16:49 +0000

    [Add]sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_11_05_035730) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.integer "level_id", default: 1
     t.integer "notice_id", default: 1
     t.string "name", null: false
